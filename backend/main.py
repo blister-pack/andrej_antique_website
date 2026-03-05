b5f66ffe9230843b8011b0e42b46f9004f7c3870
blister-pack <andrecrujo@hotmail.com>
2026-03-06 00:01:27 +0100

    item class defined

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastApi
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastApi()
+
+class Item(BaseModel):
+    title: str
+    description: str
+    price: float
+    #TODO complete w relevant categories
+
