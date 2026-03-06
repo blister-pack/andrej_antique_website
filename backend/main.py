dd8dedbf06652365f8b734f1d6ef4026b1228471
blister-pack <andrecrujo@hotmail.com>
2026-03-06 21:21:46 +0100

    fake item, class change, get function
    -added a fake item for testing
    -changed Item model a little
    -created get func

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,24 @@ from pydantic import BaseModel
 
 app = FastApi()
 
+fake_db_items = {
+    1: {
+        "title": "Grand Clock",
+        "description": "A big ol' Clock",
+        "price": 250,
+        "negotiable": False,
+    },
+}
+
+
 class Item(BaseModel):
     title: str
-    description: str
+    description: str | None = None
     price: float
-    #TODO complete w relevant categories
+    negotiable: bool
+    # TODO complete w relevant categories
 
 
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+    pass
