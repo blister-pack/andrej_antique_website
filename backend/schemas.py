910ee3151c8b4ffaec8ac1d637001b6ad349f212
blister-pack <andrecrujo@hotmail.com>
2026-03-12 13:39:16 +0100

    altered model to better represent SQL table

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,8 +3,9 @@ from sqlmodel import SQLModel, Field
 
 class Item(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    title: str
-    description: str = "womp womp"
-    price: float = 0.0
-    negotiable: bool = False
+    title: str = Field(default="Item name", index=True)
+    description: str | None = Field(default=None)
+    price: float = Field(default=0.0, index=True)
+    negotiable: bool = Field(default=False)
+    # ID is optional in Python so that the DB itself generates it
     # TODO complete w relevant categories - {date_created};
