6688e33617bd88e2855d64ffd555d78fcc463851
blister-pack <andrecrujo@hotmail.com>
2026-03-16 21:30:21 +0100

    editing schemas to separate model for table and model for item

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,11 +1,14 @@
 from sqlmodel import SQLModel, Field
 
 
-class Item(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class ItemBase(SQLModel):
     title: str = Field(default="Item name", index=True)
     description: str | None = Field(default=None)
     price: float = Field(default=0.0, index=True)
     negotiable: bool = Field(default=False)
     # ID is optional in Python so that the DB itself generates it
     # TODO complete w relevant categories - {date_created};
+
+
+class Item(ItemBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
