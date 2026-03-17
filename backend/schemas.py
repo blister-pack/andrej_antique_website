efd6787a757c8bf071a8bdb5d3f0b951a57ff0ce
blister-pack <andrecrujo@hotmail.com>
2026-03-17 21:39:14 +0100

    small change in ItemBase

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, Field
 
 class ItemBase(SQLModel):
     title: str = Field(default="Item name", index=True)
-    description: str | None = Field(default=None)
+    description: str | None = Field(default="No description available.")
     price: float = Field(default=0.0, index=True)
     negotiable: bool = Field(default=False)
     # ID is optional in Python so that the DB itself generates it
